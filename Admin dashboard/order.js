2a08e06320a29c0eaccc4ff9a11918da8ab2ef6e
Okoye Dings <100094017+OkoyeDings@users.noreply.github.com>
2024-06-03 22:43:17 +0100

    update

---
+++ Admin dashboard/order.js
@@ -0,0 +1,20 @@
+const orders = [
+    {
+        productName: 'javaScript tutorial',
+        productNumber: '85743',
+        paymentStatus: 'due',
+        status: 'pending'
+    },
+    {
+        productName: 'CSS full course',
+        productNumber: '97245',
+        paymentStatus: 'refunded',
+        status: 'declined'
+    },
+    {
+        productName: 'flexbox tutorial',
+        productNumber: '36452',
+        paymentStatus: 'paid',
+        status: 'active'
+    },
+]
